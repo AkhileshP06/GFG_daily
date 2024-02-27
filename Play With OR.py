86321330365eecee7b5dee233046dba61a2182a8
AkhileshP06 <akhileshnair2003@gmail.com>
2024-02-27 10:06:47 +0530

    27/2/24

---
+++ Play With OR.py
@@ -0,0 +1,6 @@
+class Solution:
+    def game_with_number (self, arr,  n) : 
+        #Complete the function
+        for i in range (0,n-1):
+            arr[i] = arr[i]|arr[i+1]
+        return arr
